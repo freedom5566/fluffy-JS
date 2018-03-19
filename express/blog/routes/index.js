618ca8091c1f077266b54a14b70932059c26ce16
freedom5566 <s1410231034@gms.nutc.edu.tw>
2018-03-19 17:10:58 +0800

    :art: 修改style

--- express/blog/routes/index.js
+++ express/blog/routes/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index/index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+    res.render("index/index", { title: "Express" });
 });
 
 module.exports = router;
--- express/blog/routes/users.js
+++ express/blog/routes/users.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.render('users/users', {
-    name: '安安',
-  });
+router.get("/", (req, res, next) => {
+    res.render("users/users", {
+        name: "安安",
+    });
 });
 
 module.exports = router;
