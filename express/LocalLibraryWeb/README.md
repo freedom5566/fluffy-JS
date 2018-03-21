d8511f0c87a7ab5084b452fc16e2ca89f16e4b3f
freedom5566 <s1410231034@gms.nutc.edu.tw>
2018-03-21 23:58:21 +0800

    :art: 增加一些註解

--- express/LocalLibraryWeb/README.md
+++ express/LocalLibraryWeb/README.md
@@ -1,11 +1,22 @@
 # 第一個express
 
-之後會說點心得
+說點心得(漸漸增加)
 
+### nodemon
+***
 nodemon不錯用，開發的時候每次編輯完可以不用再下一次指令跑express
 而且只要下nodemon就好，他會自動找到檔案執行
 
-mlab提供免費的mongodb           
+### mongodb
+***
+[mlab](https://mlab.com/ "url")提供免費的mongodb           
 居然還能自己選擇aws、gcp、ms
 
 我選了azure，唯一一個在美國西岸(加州，應該離台灣比較近？)，其他的都在歐洲不然就美國東岸
+
+### jade
+***
+現在改名叫pug，純科普XDD看文件想說怎不見了
+但是`express --help`裏面有jade、pug到底是改名了還是怎樣，搞得我好亂阿！             
+本篇使用*ejs*
+
--- express/LocalLibraryWeb/app.js
+++ express/LocalLibraryWeb/app.js
@@ -5,27 +5,32 @@ const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
+//引入路由
 const index = require("./routes/index");
 const users = require("./routes/users");
 
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); //設定view資料夾
+app.set("view engine", "ejs"); //使用哪種模版引擎
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//這串還看不懂......
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"))); //載入靜態文件目錄
 
+
+//使用路由資料夾
 app.use("/", index);
 app.use("/users", users);
 
 // catch 404 and forward to error handler
+// 404方法
 app.use((req, res, next) => {
     let err = new Error("Not Found");
     err.status = 404;
@@ -33,6 +38,7 @@ app.use((req, res, next) => {
 });
 
 // error handler
+// middlewares錯誤處理?
 app.use((err, req, res,next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
@@ -41,7 +47,7 @@ app.use((err, req, res,next) => {
     // render the error page
     res.status(err.status || 500);
     res.render("error");
-    //next();
+    next();
 });
 
 // set mongoose connection
